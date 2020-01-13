3a1e417e2d9cbf82d12d6b95f04720437fc32b21
jhuang429 <jhuang9240@gmail.com>
2020-01-13 13:24:26 -0500

    Update owner.rb

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,12 @@
 class Owner
-  # code goes here
-end
+  def initialize(name, species)
+    @name = name
+    @species = species
+  end
+  attr_writer :name, species
+
+  def say_species 
+    puts self.species
+  end
+
+end 
